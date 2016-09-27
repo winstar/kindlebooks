7a7ce77de1669f5419364180a58d906043c7992c
Josh Wang <winstar53@hotmail.com>
2016-09-27 10:00:55 +0800

    format

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kindlebooks
-test3
+* test1
+* test2
+* test3
--- tests/default_test.go
+++ tests/default_test.go
@@ -28,12 +28,12 @@ func Test_Main(t *testing.T) {
 	beego.Trace("testing", "TestMain", "Code[%d]\n%s", w.Code, w.Body.String())
 
 	Convey("Subject: Test Station Endpoint\n", t, func() {
-	        Convey("Status Code Should Be 200", func() {
-	                So(w.Code, ShouldEqual, 200)
-	        })
-	        Convey("The Result Should Not Be Empty", func() {
-	                So(w.Body.Len(), ShouldBeGreaterThan, 0)
-	        })
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
 	})
 }
 
